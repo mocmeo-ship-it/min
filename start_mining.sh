534a6dc9fe1186903deaafd543b66fc3e2bc9fb5
mocmeo-ship-it <91778848+mocmeo-ship-it@users.noreply.github.com>
2023-04-12 09:30:36 +0700

    Update start_mining.sh

--- start_mining.sh
+++ start_mining.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-screen -d -m -S mining bash -c "ethminer -P stratum1+tcp://[etherium address]%2e1@eth-eu1.nanopool.org:9999"
+screen -d -m -S mining bash -c "ethminer -P stratum1+tcp://0xC2853C1440Cd58082D8A5CB184462441870322d6%2e1@eth-eu1.nanopool.org:9999"
